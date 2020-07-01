2e12cb237c2716d6b58d33dd1e0028f64dac3ded
purerosefallen <78877@qq.com>
2020-07-01 15:58:28 +0800

    run together

--- src/run.ts
+++ src/run.ts
@@ -7,6 +7,7 @@ const fetcher = new Tx3Fetcher();
 async function runServer(server: string) {
 	const users = await fetcher.fetchUsersFromServer(server);
 	await fs.promises.writeFile(`./output/servers/${server}.json`, JSON.stringify(users, null, 2));
+	return users;
 }
 
 async function main() {
@@ -18,8 +19,11 @@ async function main() {
 		});
 	}
 	await fetcher.initProxies();
-	for (let server of servers) {
-		await runServer(server);
+	const userListWithServer = await Promise.all(servers.map(runServer));
+	const allServersList: any = {};
+	for (let i = 0; i < servers.length;++i) {
+		allServersList[servers[i]] = userListWithServer[i];
 	}
+	await fs.promises.writeFile(`./output/all.json`, JSON.stringify(allServersList, null, 2));
 }
 main();
